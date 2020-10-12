d36c00e1fcfd0ae19f7caf940456b18987f0ff4a
Mateusz Dudek <mateusz.dudek@predicagroup.com>
2020-10-12 16:20:58 +0200

    Add base project

---
+++ src/MyWebApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyWebApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
